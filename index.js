44548fff498b7b3d153fcc8790f5562fb81f61e7
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2023-10-16 13:34:17 -0400

    Updated storeController to have 3 more functions to add, remove and show the cart data. As well ad updated update function to update the cart data with the store data when a game is updated

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { readJSONFile, writeJSONFile } = require('./src/helpers');
 const games = readJSONFile('./data', 'store.json');
-const {create,deleteGame,update, searchByID} = require("./src/storeController");
+const cartFile = readJSONFile('./data','cart.json');
+
+const {create,deleteGame,update, searchByID,addToCart,removeFromCart,viewCart} = require("./src/storeController");
 // create an alias called inform to store the console.log function
 // When providing user feedback in the terminal use `inform`
 // When developing/debugging use `console.log`
@@ -15,45 +17,54 @@ function run() {
   const action = process.argv[2];
   const gamesInput = process.argv[3];
   const realesedInput = process.argv[4];
-  const age_rateing =process.argv[5];
+  const age_rating =process.argv[5];
   switch (action) {
     case 'showGames':
-      print(action,games);
-      break;
+        print(action,games);
+        break;
     case 'create':
-    updatedStore = create(games, gamesInput,realesedInput,age_rateing);
-    writeToFile = true;
-    break;
+        updatedStore = create(games, gamesInput,realesedInput,age_rating);
+        writeToFile = true;
+        break;
     case 'searchByID':
         const searchgame = searchByID(games, gamesInput); // gamesInput is the id 
         print(searchgame);
         break;
     case 'update':
-        updatedStore = update(games, gamesInput,realesedInput,age_rateing);
+        print(process.argv)
+        const [, , cmd ,ID,name,realesed, rating] = process.argv;
+        [updatedStore, updatedCart] = update(games, cartFile, ID, name, realesed,rating);
         writeToFile = true;
+        writeTOCart = true;
         break;
     case 'deleteGame':
-      updatedStore = deleteGame(games, gamesInput);
-      writeToFile = true;
-      break;
-    case 'AddTocart':
-      print(action);
-      break;
+        updatedStore = deleteGame(games, gamesInput);
+        writeToFile = true;
+        break;
+    case 'addToCart':
+        updatedCart = addToCart(games,gamesInput)
+        writeTOCart = true;
+        break;
+    case 'removeFromCart':
+        updatedCart = removeFromCart(cartFile,gamesInput);
+        writeTOCart = true;
+        break;
     case 'showCart':
-      //showCart()  
-      /* maybe use while loop to add to array that has accumulator useing proccess.argsv[accumulator]
-       to continue add games to the cart and show final price at the end where they will ether type
-        buy or cancel to exit*/
-      break;  
+        viewCart(cartFile)
+        break; 
     default:
-      print('There was an error.');
+        print('There was an error.');
   }
   if (writeToFile) {
     writeJSONFile('./data', 'store.json', updatedStore);
   }
-  if (writeToFile) {
-    writeJSONFile('./data', 'store.json', updatedStore);
+  if (writeTOCart) {
+    writeJSONFile('./data', 'cart.json', updatedCart);
   }
 }
 
 run();
+      //showCart()  
+      /* maybe use while loop to add to array that has accumulator useing proccess.argsv[accumulator]
+       to continue add games to the cart and show final price at the end where they will ether type
+        buy or cancel to exit*/
--- src/storeController.js
+++ src/storeController.js
@@ -1,8 +1,13 @@
 const { nanoid } = require('nanoid');
 const gamePrice = require('../data/gamePrice.json');
 const print = console.log;
+const { readJSONFile, writeJSONFile } = require('./helpers'); 
 
-function create(store, gameName,Released,age_rat) {
+const cartFile = 'cart.json';
+
+let cart = readJSONFile('./data', cartFile);
+
+  function create(store, gameName,Released,age_rat) {
     const game = {
       name: gameName,
       // Limit the id to be 4 characters long by placing a 4 as the argument for nanoid
@@ -14,6 +19,7 @@ function create(store, gameName,Released,age_rat) {
     store.push(game);
     return store;
   }
+
   function deleteGame(store, qrID2) {
     const index = store.findIndex((game) => game.qrID === qrID2.trim());
     if (index > -1) {
@@ -25,36 +31,95 @@ function create(store, gameName,Released,age_rat) {
       return store;
     }
   }
-  function update(store, qrID2, newGame,realesedInput, age_rat) {
+
+  function update(store, cart, qrID2, newGame, releasedInput, age_rating) {
+    const isReleased = releasedInput === "true" ? true : false;
     const index = store.findIndex((game) => game.qrID === qrID2);
+    const indexCart = cart.findIndex((game) => game.qrID === qrID2);
+  
     if (index > -1) {
-        store[index].qrID = qrID2;
-        store[index].name = newGame;
-        store[index].price_in_cents = gamePrice[newGame] || 6000;
-        if(realesedInput){
-            store[index].released = true;
-        }else{
-            store[index].released = false;
-        }
-        store[index].age_rating = age_rat || store[index].age_rating
-        print(store[index]);
-        print('game successfully updated');
-      return store;
+      store[index].qrID = qrID2;
+      store[index].name = newGame;
+      store[index].price_in_cents = gamePrice[newGame] || 6000;
+      store[index].released = isReleased;
+      store[index].age_rating = age_rating || "E";
+    
+  
+      print(store[index]);
+      print('Game successfully updated in the store');
+  
+      if (indexCart > -1) {
+        cart[indexCart].qrID = qrID2;
+        cart[indexCart].name = newGame;
+        cart[indexCart].price_in_cents = gamePrice[newGame] || 6000;
+        cart[indexCart].released = isReleased;
+        cart[indexCart].age_rating = age_rating || "E"
+        
+  
+        print(cart[indexCart]);
+        print('Game successfully updated in the cart');
+      }
+  
+      return [store, cart];
     } else {
-      print('game not found. No action taken');
-      return store;
+      print('Game not found in the store. No action taken');
+      return [store, cart];
     }
   }
-
-function searchByID(store, qrID2) {
+  
+  
+  function searchByID(store, qrID2) {
     const game = store.find((game) => game.qrID === qrID2);
     return 'qrID:'+game.qrID + '  Name:' + game.name + '  Rating:'+ game.age_rating+ '  price: $' + game.price_in_cents/100 ;
   }
+
+  function addToCart(store, gameName) {
+    const game = store.find((game) => game.name === gameName);
+    if (game) {
+      cart.push(game);
+      console.log(`Added ${game.name} to the cart.`);
+    }else {
+      console.log(`Game ${gameName} not found.`);
+    }
+    return cart; 
+  }
+  function removeFromCart(cart, gameName) {
+    const gameIndex = cart.findIndex((game) => game.name === gameName);
+    if (gameIndex !== -1) {
+      cart.splice(gameIndex, 1);
+      console.log(`Removed ${gameName} from the cart.`);
+    } else {
+      console.log(`Game ${gameName} not found in the cart.`);
+    }
+    return cart;
+  }
+
+  function calculateTotalPrice(cart){
+    const totalPrice = cart.reduce((total, game) => total + game.price_in_cents, 0);
+    return totalPrice / 100; 
+  }
+  
+  function viewCart(cart){
+    if (cart.length === 0) {
+      console.log("Your cart is empty.");
+    } else {
+      console.log("Cart Contents:");
+      cart.forEach((game) => {
+      console.log(`Name: ${game.name}, Price: $${game.price_in_cents / 100}`);
+      });
+      console.log(`Total Price: $${calculateTotalPrice(cart)}`);
+    }
+  }
+
+
 
 
 module.exports = {
     create,
     deleteGame,
     update,
-    searchByID
+    searchByID,
+    addToCart,
+    removeFromCart,
+    viewCart
 };
